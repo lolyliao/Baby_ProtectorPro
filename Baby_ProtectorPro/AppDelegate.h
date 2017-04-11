b9bb848851d439eaa5f4c475c62b60ec1f0ab1cf
Macx <Macx@macxdemacbook-pro-652.local>
2017-04-11 10:06:36 +0800

    Initial Commit

---
+++ Baby_ProtectorPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Baby_ProtectorPro
+//
+//  Created by Macx on 2017/4/11.
+//  Copyright © 2017年 Baby_Protector. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Baby_ProtectorPro/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Baby_ProtectorPro
+//
+//  Created by Macx on 2017/4/11.
+//  Copyright © 2017年 Baby_Protector. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
